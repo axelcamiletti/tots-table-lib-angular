5a0f1e62c0f60302c4a3de8484001dfe442c3794
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-16 23:29:28 -0300

    Add witdh function in TD.

--- projects/example/src/app/pages/table/table.component.ts
+++ projects/example/src/app/pages/table/table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TotsListResponse } from '@tots/core';
-import { StringColumnComponent, TotsActionTable, TotsTableConfig } from 'projects/tots/table/src/public-api';
+import { MoreMenuColumnComponent, StringColumnComponent, TotsActionTable, TotsTableConfig } from 'projects/tots/table/src/public-api';
 import { delay, of } from 'rxjs';
 
 @Component({
@@ -22,7 +22,11 @@ export class TableComponent implements OnInit {
   loadConfig() {
     this.config.id = 'table-example';
     this.config.columns = [
-      { key: 'title', component: StringColumnComponent, title: 'Titulo', field_key: 'title' }
+      { key: 'title', component: StringColumnComponent, title: 'Titulo', field_key: 'title' },
+      { key: 'more', component: MoreMenuColumnComponent, title: '', extra: { width: '60px', actions: [
+        { icon: 'add', title: 'Editar', key: 'edit' },
+        { icon: 'add', title: 'Eliminar', key: 'remove' },
+      ]} },
     ];
 
     let data = new TotsListResponse();
